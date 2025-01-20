076f96ac81ff484ce87b9809b33a1821f05ee5ed
Jean-9 <80298133+Jean-9@users.noreply.github.com>
2025-01-20 10:29:19 -0300

    Update index.php
    atualização do código para inserir os novos dados

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <html>
-
 <head>
-<title>Exemplo PHP</title>
+    <title>Exemplo PHP</title>
 </head>
 <body>
 
@@ -9,9 +8,7 @@
 ini_set("display_errors", 1);
 header('Content-Type: text/html; charset=iso-8859-1');
 
-
-
-echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
+echo 'Versão Atual do PHP: ' . phpversion() . '<br>';
 
 $servername = "54.234.153.24";
 $username = "root";
@@ -19,30 +16,45 @@ $password = "Senha123";
 $database = "meubanco";
 
 // Criar conexão
-
-
 $link = new mysqli($servername, $username, $password, $database);
 
-/* check connection */
+/* Verificar conexão */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
 
-$valor_rand1 =  rand(1, 999);
-$valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
-$host_name = gethostname();
+// Gerar valores randômicos e estáticos para os campos
+$AlunoID = rand(1, 999);
+$Nome = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+$Sobrenome = strtoupper(substr(bin2hex(random_bytes(4)), 1));
+$Endereco = "Rua Exemplo, " . rand(1, 1000);
+$Cidade = "Cidade " . chr(rand(65, 90));
+$Estado = "Estado " . chr(rand(65, 90));
+$Pais = "Pais Exemplo";
+$CEP = rand(10000, 99999) . "-" . rand(100, 999);
+$DataNascimento = date('Y-m-d', strtotime('-' . rand(18, 30) . ' years'));
+$Email = strtolower($Nome . "." . $Sobrenome . "@exemplo.com");
+$Telefone = "+55 " . rand(10, 99) . " 9" . rand(1000, 9999) . "-" . rand(1000, 9999);
+$Curso = "Curso " . chr(rand(65, 90));
+$DataMatricula = date('Y-m-d');
+$StatusMatricula = "Ativo";
+$Host = gethostname();
 
-
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
-
+// Query de inserção
+$query = "INSERT INTO dados (
+    AlunoID, Nome, Sobrenome, Endereco, Cidade, Estado, Pais, CEP, DataNascimento, Email, Telefone, Curso, DataMatricula, StatusMatricula, Host
+) VALUES (
+    '$AlunoID', '$Nome', '$Sobrenome', '$Endereco', '$Cidade', '$Estado', '$Pais', '$CEP', '$DataNascimento', '$Email', '$Telefone', '$Curso', '$DataMatricula', '$StatusMatricula', '$Host'
+)";
 
 if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
+    echo "Novo registro criado com sucesso.";
 } else {
-  echo "Error: " . $link->error;
+    echo "Erro: " . $link->error;
 }
 
 ?>
 </body>
 </html>
+
